0f655d4071d8c01c3258751164099681114ad17e
Svandís <svandis13@gmail.com>
2019-06-18 21:27:30 +0200

    Added a code for plotting of weight for each breed in a ggplot

--- hundur.R
+++ hundur.R
@@ -1,1 +1,6 @@
-#vuf vuf
+#Plot weight for each breed in a ggplot
+df_data_raw <- data.frame(sex =data_raw@phdata$sex, weight =data_raw@phdata$Body_weight, Bread = data_raw@phdata$Breed)
+
+ggplot(df_data_raw,aes(x=Breed, y=weight)) +
+  geom_boxplot()
+
