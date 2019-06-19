c6f567cc293f90e3b335026fa350acff3b77c0df
Svandís <svandis13@gmail.com>
2019-06-19 09:03:39 +0200

    Added tidyverse pipe instead of greating a dataframe

--- hundur.R
+++ hundur.R
@@ -1,6 +1,15 @@
 #Plot weight for each breed in a ggplot
-df_data_raw <- data.frame(sex =data_raw@phdata$sex, weight =data_raw@phdata$Body_weight, Bread = data_raw@phdata$Breed)
 
-ggplot(df_data_raw,aes(x=Breed, y=weight)) +
-  geom_boxplot()
+data_raw@phdata %>% 
+  select(Breed, Body_weight) %>% 
+  na.omit() %>% 
+  ggplot(aes(x=Breed, y=Body_weight, fill=Breed)) +
+  geom_boxplot() +
+  geom_jitter(width = 0.2, alpha=0.3) +
+  labs(title="Weight distribution for all breeds",
+       subtitle="RaukR project 2019",
+       caption="Sources: PLoS ONE, 2015")+
+  theme_bw() + 
+  theme(legend.position = "none")
+
 
