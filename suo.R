2a5658334f55df1d0ea41b83f172b4ec935ad3ec
sinkevid <51613684+sinkevid@users.noreply.github.com>
2019-06-19 14:24:17 +0200

    updated my scripts

--- suo.R
+++ suo.R
@@ -1,48 +1,11 @@
 # Dovile's functions & data analysis
 
-## used for dogs phenotype summary
-funm <- function(x){mean(x, na.rm = T)}
-funsd <- function(y){sd(y, na.rm = T)}
-
-
 # general info on the participants of the study
 length(unique(data_raw@phdata$Country)) ## data from 5 countries
 length(unique(data_raw@phdata$Breed)) ## data covers 9 dog breeds
 
+library(tidyverse)
 
-# country with the most dogs
-res1 <- data_raw@phdata %>% 
-  group_by(Country) %>%
-  summarize(total_dogs = sum(n())) %>%
-  arrange(desc(total_dogs))
-print(res1)
-
-ggplot(data= res1, aes(x = "", y = total_dogs, fill = Country))+
-  geom_bar(width = 1, stat = "identity")+
-  coord_polar("y")+
-  labs(title="Dogs by country",
-       subtitle="RaukR project 2019")+
-  theme_bw()+
-  theme(axis.title.x = element_blank(),
-        axis.title.y=element_blank())
-  
-
-# summary of dog breeds per country
-data_raw@phdata %>%
-  group_by(Breed, Country) %>%
-  summarize(n = n()) %>%
-  arrange(desc(n)) %>%
-  arrange(desc(Country))
-
-
-# top1 breed per country
-data_raw@phdata %>%
-  group_by(Breed, Country) %>%
-  summarize(n = n()) %>%
-  group_by(Country)%>%
-  slice(which.max(n))
-  
-  
 # number of dogs per breed (male, female)
 dogs <- data_raw@phdata %>%
   group_by(Breed, sex) %>%
@@ -50,30 +13,6 @@ dogs <- data_raw@phdata %>%
   group_by(Breed) %>%
   spread(key="sex", value = "n" ) %>%
   mutate_all(~replace(., is.na(.), 0))
-
-dogs$Breed_name <- c("Boxer",
-                      "Belgian Shepperd",
-                      "Cavalier King Charles spaniel",
-                      "Dachshund", 
-                      "Doberman pinscher",
-                      "Finnish lapphund",
-                      "German Shepperd",
-                      "Labrador retriever", 
-                      "Newfoundland")
-
-colnames(dogs)<- c("Abbreviation", "Female", "Male", "Breed")
-dogs[,c("Breed", "Abbreviation", "Female", "Male")]
-
-
-
-
-
-
-
-
-
-
-
 
 dogs$Breed_name <- c("Boxer",
                      "Belgian Shepperd",
@@ -88,7 +27,7 @@ dogs$Breed_name <- c("Boxer",
 colnames(dogs)<- c("Abbreviation", "Female", "Male", "Breed")
 dogs[,c("Breed", "Abbreviation", "Female", "Male")]
 
-
+# Info on dogs per country
 country_info <- data_raw@phdata %>% 
   group_by(Country) %>%
   summarize(total_dogs = sum(n())) %>%
@@ -97,3 +36,24 @@ country_info <- data_raw@phdata %>%
 country_info$fullname <- c("Finland", "Belgium", "Sweden", "France", "Denmark") 
 colnames(country_info)<- c("Abbreviation", "Total number of dogs", "Country")
 country_info[,c("Country", "Abbreviation", "Total number of dogs")]
+
+# Table of mean dog weight and age
+# mean ans sd functions
+funm <- function(x){mean(x, na.rm = T)}
+
+#table
+dogsWH <- data_raw@phdata %>%
+  group_by(Breed, sex) %>%
+  summarize(n = n(),
+            mean.Age = funm(Age), 
+            mean.Weight = funm(Body_weight)) 
+
+dogsWH$sex[dogsWH$sex == 0]<- "female"
+dogsWH$sex[dogsWH$sex == 1]<- "male"
+
+print(dogsWH)
+
+
+
+  
+  
